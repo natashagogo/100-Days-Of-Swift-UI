bd29c85660d0e5ea8031ec4bef7d60f52c05139f
Natasha Godwin <natashasgodwin@gmail.com>
2020-04-06 19:22:07 -0400

    Finished Guess The Flag
    - Added flags
    - Created methods for tracking the user's score and resetting the game
    - Added basic styling

--- 03 - Guess The Flag/Guess The Flag/ContentView.swift
+++ 03 - Guess The Flag/Guess The Flag/ContentView.swift
@@ -9,8 +9,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Russia",
+        "Spain",
+        "UK",
+        "US"
+    ].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var alertTitle = ""
+    @State private var score = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            // Background
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of...")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    Text(countries[correctAnswer].uppercased())
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0..<3) { index in
+                    Button(action: {
+                        self.flagTapped(index)
+                    }) {
+                        Image(self.countries[index])
+                            .renderingMode(.original) // tells SwiftUI to render the original image (instead of recoloring them as a button)
+                    }
+                    .clipShape(Capsule())
+                    .overlay(Capsule().stroke(Color.gray, lineWidth: 0.5))
+                    .shadow(color: .black, radius: 2, x: 2, y: 3)
+                    
+                }
+            }
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text(alertTitle), message: Text("You've earned \(score) point(s)."), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ index: Int) {
+        if index == correctAnswer {
+            alertTitle = "Nice work! You earned one point."
+            score += 1
+        } else {
+            alertTitle = "Nope! Better luck next time!"
+            score -= 1
+        }
+        
+        if score < 0 {
+            score = 0
+        }
+        
+        showingScore = true
+    }
+    
+    // Reset the game
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
