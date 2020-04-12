a4534ee5d1c616b360166acffc17e64a4a70e48a
Natasha Godwin <natashasgodwin@gmail.com>
2020-04-12 16:11:58 -0400

    Re-Factored Code
    * Created custom views for the messages and message history
    * Added conditional modifiers to change UI according to who sent the message

--- 00 - Practice Projects/04 - Message Stacks/Message Stacks/ContentView.swift
+++ 00 - Practice Projects/04 - Message Stacks/Message Stacks/ContentView.swift
@@ -8,31 +8,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
+// TO DO
+// Learn how to create custom shapes to create a more attractive message UI.
+// Create custom modifiers ?
+
+
+struct Message: View {
+    var profilePic: String
+    var message: String
+    var isUser: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: isUser ? .leading : .trailing) {
+            isUser ? Color.blue : Color.white
+            HStack {
+                Image(systemName: profilePic)
+                Text(message)
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+        }
+        .font(isUser ? .body : .title)
+        .foregroundColor(isUser ? Color.white : Color.blue)
+        .frame(width: 350, height: 100)
+    }
+}
+
+
+struct MessageHistory: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "person.fill")
-                Text("Hello, World!")
-                Spacer() // pushes HStack to the left
-            }
-            .padding()
-            .font(.title)
-            
-            HStack {
-              Spacer() // pushes HStack to the right
-              Image(systemName: "globe")
-              Text("Hello, Natasha!")
-            }
-            .padding()
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-            
-            Spacer() // pushes VStack to the top
-            
+            Message(profilePic: "person.fill", message: "Hello, World!", isUser: true)
+            Message(profilePic: "globe", message: "Hello, Natasha!", isUser: false)
+            Spacer()
         }
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 220/255, green: 220/255, blue: 230/255)
+                .edgesIgnoringSafeArea(.all)
+            MessageHistory()
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
