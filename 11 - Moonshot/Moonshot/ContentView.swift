fc8a2cfc3da94935e96f4f8df33a248bfec4e335
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-01 21:00:12 -0400

    Added Files For Moonshot

---
+++ 11 - Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Natasha Godwin on 6/1/20.
+//  Copyright © 2020 Natasha Godwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
