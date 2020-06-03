1019045f40fc9ca5a16dc3324a6953d120c2f4a0
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-02 21:37:13 -0400

    Added JSON files

--- 11 - Moonshot/Moonshot/ContentView.swift
+++ 11 - Moonshot/Moonshot/ContentView.swift
@@ -8,18 +8,9 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(destination: Text("Detail \(row)")) {
-                    Text("Row \(row)")
-                }
-            }
-            .navigationBarTitle("Hello, SwiftUI")
-        }
+        Text("Hello, World")
     }
 }
 
