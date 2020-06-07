9552527bd9fc166b78eea779013f10990139af50
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-07 12:29:35 -0400

    Initial Files

---
+++ 12 - Drawing/Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Natasha Godwin on 6/7/20.
+//  Copyright © 2020 Natasha Godwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
