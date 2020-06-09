c9bf3571866328ef5427f14acb924c409e53f48e
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-08 20:37:25 -0400

    Cleaned Up Files
    Put the Color Cycling example in a separate file.

--- 12 - Drawing/Drawing/ContentView.swift
+++ 12 - Drawing/Drawing/ContentView.swift
@@ -8,32 +8,6 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-    
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-            }
-        }.drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
 
 struct ContentView: View {
     @State private var colorCycle = 0.0
