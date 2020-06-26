cc667946d81abc71a41078129d52b4f0c35343c6
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-25 22:29:53 -0400

    Added Initial Files for CupcakeCorner

---
+++ 14 - CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Natasha Godwin on 6/25/20.
+//  Copyright © 2020 Natasha Godwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
