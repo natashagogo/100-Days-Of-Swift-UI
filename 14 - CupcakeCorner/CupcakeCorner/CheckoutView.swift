fa06642cf19c1931904be20a9d277fab6d3590ea
Natasha Godwin <natashasgodwin@gmail.com>
2020-06-28 22:44:06 -0400

    Built UI for CheckoutView

--- 14 - CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ 14 - CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,7 +11,25 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
     }
 }
 
