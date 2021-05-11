988c15a3866f0fdd6295771dcaa18b7084c974a3
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-11 17:11:02 -0400

    Built Guess The Flag
    Created a basic quiz app that tests people's knowledge of country flags.

--- 03 - GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 03 - GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,11 +7,78 @@
 
 import SwiftUI
 
+/*
+ Int.random(in: ) - a method that generates a random number within a range
+ 
+ .renderingMode(.original) - a modifier that tells SwiftUI to render an image in a button with the original pixels rather the default blue for buttons in iOS
+ */
+
 
 struct ContentView: View {
+    @State private var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Russia",
+        "Spain",
+        "UK",
+        "US"
+    ].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     @State private var showingAlert = false
     var body: some View {
-        Text("Guess the Flag")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of:")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule()) // round the edges
+                            .overlay(Capsule().stroke(Color.blue, lineWidth: 0.5)) // add a border
+                            .shadow(color: .blue, radius: 2)
+                    }
+                }
+            }
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text("\(scoreTitle)"), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    // Reset the game
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
