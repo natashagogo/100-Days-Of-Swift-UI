413df4f6f77cca41ef953c3c716bf35ca0d71b80
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-13 18:40:44 -0400

    View Order Demo

---
+++ 00 - Practice Projects/04 - ViewOrderDemo/ViewOrderDemo/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  ViewOrderDemo
+//
+//  Created by Natasha Godwin on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .background(Color.white)
+            .padding()
+            .background(Color.white)
+            .padding()
+            .background(Color.blue)
+            .padding()
+            .background(Color.white)
+            .padding()
+            .background(Color.blue)
+            .padding()
+            .background(Color.blue)
+            .padding()
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00 - Practice Projects/04 - ViewOrderDemo/ViewOrderDemo/ViewOrderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewOrderDemoApp.swift
+//  ViewOrderDemo
+//
+//  Created by Natasha Godwin on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewOrderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
