9750694626c44fa3a46c103a8e7d1ee247d1ea68
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-13 18:37:57 -0400

    ViewsAndModifiers Initial Files

---
+++ 04 - ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Natasha Godwin on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
