d674d57e99cb9a356d5791fedc9c8d6ae58dc84c
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-19 18:32:16 -0400

    BetterRest Initial Files
    Got an overview of the project and tried out some of the techniques. Learned how to create a Stepper and manipulate dates with DatePicker, Date, and DateComponents.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Natasha Godwin on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Natasha Godwin on 5/19/21.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var sleepAmount: Double = 8.0
+    @State private var wakeUpTime = Date()
+    var body: some View {
+        NavigationView {
+            Form {
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                DatePicker("Please choose a date", selection: $wakeUpTime, in: Date()...)
+                    .labelsHidden()
+            }
+            .navigationTitle("BetterRest")
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
