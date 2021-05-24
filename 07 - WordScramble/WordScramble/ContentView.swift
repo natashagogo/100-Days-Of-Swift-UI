a9c4f914d0e8ed38fa62f5d063005e2380de8fcc
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-24 17:59:32 -0400

    Finished
    - Loaded a list of starter words into the app
    - Displayed a random word from the list in the NavigationBarTitle
    - Added validation to prevent the user from entering non- and duplicate words.

--- 07 - WordScramble/WordScramble/ContentView.swift
+++ 07 - WordScramble/WordScramble/ContentView.swift
@@ -7,119 +7,15 @@
 
 import SwiftUI
 
-/*
- 
- Working with Lists
- 
- List v. Form
- Both List and Form can contain a mix of static and dynamic data. However, there's one major difference:
- With List, you don't need to use ForEach to generate rows for dynamic data. (If you want to mix static and dynamic data, you'll still need to use ForEach)
- 
- List(0..<5) {
-   Text("Row \($0)")
- }
- 
- Ids
- When generating a list from an array of strings or numbers, each item needs to have an id, regardless of whether you're using ForEach or List
- 
- List(list, id: \.self) {
-    Text($0)
- }
- 
- ForEach(list, id: \.self) {
-    Text($0)
- }
- 
- .listStyle(GroupedListStyle())
- 
- Working with Files
- 
- Swift knows to look for images in the asset catalogue, but with other types of data, it gets more complicated.
- 
- If you want to use data from an external file - JSON, TXT, CSV - there are more steps involved.
- 
- Bundle
- A single directory that contains an app's compiled code, Info.plist file, asset catalogue, and other files.
- 
- URL
- A data type that can store the location of files.
- 
- Bundle.main.url()
- This code is used to access files inside your main app bundle. This is an optional URL: if the file exists, you'll get it; if it doesn't, you'll get nil.
- 
- if let fileURL = Bundle.main.url(forResource: "name-of-file", withExtension: "txt") {
-   // success!
- }
- 
- String(contentsOf:)
- Accepts a file URL and converts its contents into a string.
- 
- Note that if the file can't be loaded, you'll get an error.
- 
- if let fileContents = try? String(contentsOf: fileURL) {
-   
- }
- 
- Working with Strings
- 
- iOS have powerful APIs for working with strings.
- 
- components(separatedBy:)
- A method that converts a single string into an array of strings. Useful for converting text files into arrays of strings.
- 
- .randomElement()
- A method that returns a random element from an array. Returns an optional string, so must be unwrapped or used with nil coalescing.
- 
- .trimmingCharacters(in:)
- A method that is used to remove certain kinds of characters, usually whitespace and new lines, from the start and end of a string.
- 
- UITextChecker
- A class that is used for spell check. Comes from UIKit and is written in Objective-C.
- 
- There are four steps to using it.
- 1. Create a word and an instance of UITextChecker to check it.
- 
- let word = "word"
- let spellCheck = UITextChecker()
- 
- 2. Tell the checker how much of the string it needs to check.
- 
- Swift and Objective-C, the language used to create UITextChecker, don't handle strings the same way. Because of that, you need to create an Objective-C string range using the entire length of your characters.
- 
- let range = NSRange(location: 0, length: word.utf16.count)
- 
-3. Get the location of the misspellings
- 
- let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
- 
- 4. Check for spelling mistakes
- 
- Objective-C doesn't have optionals. Instead, it uses a special type called NSNotFound to represent missing data.
- 
- So, to check for mistakes, you'll need to write this:
- 
- let zeroMistakes = misspelledRange.location == NSNotFound
- 
- 
- Styling TextFields
- 
- .textFieldStyle(RoundedBorderTextFieldStyle())
- This modifier will give a TextView a light gray border with rounded corners.
- 
- onCommit
- 
- A parameter in TextField that calls a closure whenever the user presses return
- 
- 
- 
- 
- 
- */
 
 struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationView {
@@ -134,22 +30,91 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
         }
+    }
+    
+    func startGame() {
+        // 1. Find start.txt
+        if let starterWordsFile = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let starterWords = try? String(contentsOf: starterWordsFile) {
+                // 3. Split the string into an array of strings
+                let allWords = starterWords.components(separatedBy: "\n")
+                // 4. Get a random word (or otherwise use silkworm)
+                rootWord = allWords.randomElement() ?? "silkworm"
+                // Exit, if successful
+                return
+            }
+        }
+
+        // If there's an error loading the file, make the app crash.
+        fatalError("Could not load start.txt from bundle.")
     }
     
     func addNewWord() {
         // Lowercase and trim the word
-        let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         // Exit if the string is empty
-        guard word.count > 0 else {
+        guard answer.count > 0 else {
            return
         }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "Try something new!", message: "You already used that word.")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Say what?", message: "Are you sure that's a word?")
+            return
+        }
+        
+        guard isEnglish(word: answer) else {
+            wordError(title: "Not in this English", message: "That isn't a word in the English language.")
+            return
+        }
+        
         // Add word to usedWords array
-        usedWords.insert(word, at: 0)
+        usedWords.insert(answer, at: 0)
         newWord = ""
         
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var starterWordCopy = rootWord
+        
+        for letter in word {
+            if let position = starterWordCopy.firstIndex(of: letter) {
+                starterWordCopy.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isEnglish(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let errorFound = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return errorFound.location == NSNotFound
+        
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
