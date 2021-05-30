22b56e99e1932313043cf2c4e95e0f4c38da4f44
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-30 15:41:53 -0400

    Initial Files

---
+++ 09 - MultiplyThis/MultiplyThis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplyThis
+//
+//  Created by Natasha Godwin on 5/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 09 - MultiplyThis/MultiplyThis/MultiplyThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyThisApp.swift
+//  MultiplyThis
+//
+//  Created by Natasha Godwin on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
