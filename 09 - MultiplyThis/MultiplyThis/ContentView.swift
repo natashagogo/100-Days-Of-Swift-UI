fcd350566b789b3bae40ab3e138c2cadf9e8772d
Natasha Godwin <natashasgodwin@gmail.com>
2021-05-30 16:38:39 -0400

    Built Settings
    In ContentView, the user can toggle between the Settings and Game views. Today, I fleshed out the Settings UI and functionality. Users can choose multiplication tables to practice and the number of questions they want to be asked.

--- 09 - MultiplyThis/MultiplyThis/ContentView.swift
+++ 09 - MultiplyThis/MultiplyThis/ContentView.swift
@@ -7,10 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
+/*
+ 
+ This app needs ...
+ 
+ - Two views (one for settings, the other for the game)
+ - A way to select multiplication tables to practice (buttons, stepper)
+ - A way to choose the number of questions (5, 10, 20, or “All”)
+ - A randomly generated set of questions, within the parameters set by the user
+
+ */
+
+struct Game: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Let's Play!")
+    }
+}
+
+struct Settings: View {
+    @State private var tableSelected = 2
+    @State private var questionNumber = 0 // index
+    
+    let numberOfQuestions = ["5", "10", "20", "All"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Practice")) {
+                    Stepper("\(tableSelected) Tables", value: $tableSelected, in: 1...12)
+                }
+                Section(header: Text("Number of Questions")) {
+                    Picker("", selection: $questionNumber) {
+                        ForEach(0..<numberOfQuestions.count) { number in
+                            Text("\(numberOfQuestions[number])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var isActive = false
+    var body: some View {
+        VStack {
+            if isActive {
+                Game()
+            } else {
+                Settings()
+            }
+            Button(action:  {
+                self.isActive.toggle()
+            }) {
+                Text("\(isActive ? "Settings": "Play")")
+            }
+        }
+        
     }
 }
 
