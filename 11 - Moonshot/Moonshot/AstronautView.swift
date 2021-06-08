264c6fc43be4670ad8c9f3599ef053b539d95bab
Natasha Godwin <natashasgodwin@gmail.com>
2021-06-08 18:44:07 -0400

    Attempted Challenge #2

--- 11 - Moonshot/Moonshot/AstronautView.swift
+++ 11 - Moonshot/Moonshot/AstronautView.swift
@@ -10,6 +10,31 @@ import SwiftUI
 struct AstronautView: View {
     let astronaut: Astronaut
     
+    /*
+     This was my attempt to complete the following challenge:
+      Modify AstronautView to show all the missions this astronaut flew on.
+     Unfortunately, it doesn't work. Coming back to this one later.
+     
+     let missions: [Mission]
+     
+     init(astronaut: Astronaut, missions: [Mission]) {
+         self.astronaut = astronaut
+         var matches = [Mission]()
+         
+         for mission in missions {
+             for member in mission.crew {
+                 if astronaut.id == member.name {
+                     matches.append(mission)
+                 } else {
+                     fatalError("No missions found.")
+                 }
+             }
+         }
+         self.missions = matches
+     }
+     
+     */
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical) {
@@ -22,6 +47,10 @@ struct AstronautView: View {
                     Text(self.astronaut.description)
                         .padding()
                         .layoutPriority(1)
+                    
+                    /*ForEach(missions) { mission in
+                        Text("\(mission.displayName)")
+                    }*/
                 }
             }
         }
@@ -31,6 +60,7 @@ struct AstronautView: View {
 
 struct AstronautView_Previews: PreviewProvider {
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    /*static let missions: [Mission] = Bundle.main.decode("missions.json")*/
     static var previews: some View {
         AstronautView(astronaut: astronauts[0])
     }
