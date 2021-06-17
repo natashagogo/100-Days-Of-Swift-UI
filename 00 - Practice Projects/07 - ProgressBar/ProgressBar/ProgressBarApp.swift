f3ca64768c8589edb01217db6faa7c7585938a61
Natasha Godwin <natashasgodwin@gmail.com>
2021-06-17 13:02:14 -0400

    Built a progress bar

---
+++ 00 - Practice Projects/07 - ProgressBar/ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by Natasha Godwin on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
