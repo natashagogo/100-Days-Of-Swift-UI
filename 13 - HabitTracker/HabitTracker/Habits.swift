1213e5d7f5a608bf831b0f9a26116f0e5bd3dd03
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-02 15:36:17 -0400

    Added alternative data structures in comments

--- 13 - HabitTracker/HabitTracker/Habits.swift
+++ 13 - HabitTracker/HabitTracker/Habits.swift
@@ -19,3 +19,46 @@ struct Habit: Identifiable {
 class HabitList: ObservableObject {
     @Published var list = [Habit]()
 }
+
+/*
+ Alternative data structures
+ 
+ 1. Turn habit into a class
+ 
+ struct HabitInfo: Identifiable {
+     let id = UUID()
+     var name: String
+     var goal: String
+     var metric: String
+     var frequency: String
+     var progress: Int
+ }
+ 
+ class Habit: ObservableObject {
+    @Published var habit = HabitInfo(name: "", goal: "", metric: "count", frequency: "daily", progress: 0)
+ }
+
+ class HabitList: ObservableObject {
+     @Published var list = [Habit]()
+ }
+ 
+ 2. Create one big class
+ 
+ class Habit: ObservableObject {
+     let id = UUID()
+     @Published var name: String
+     @Published var goal: String
+     @Published var metric: String
+     @Published var frequency: String
+     @Published var progress: Int
+ 
+    init(name: String, goal: String, metric: String, frequency: String, progress: Int) {
+        self.name = name
+        self.goal = goal
+        self.metric = metric
+        self.frequency = frequency
+        self.progress = progress
+    }
+ }
+ 
+ */
