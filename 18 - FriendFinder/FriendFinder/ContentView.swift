6dff0346f53420de13f48236383cc43010978c36
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-02 16:05:51 -0400

    Initial Files

---
+++ 18 - FriendFinder/FriendFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFinder
+//
+//  Created by Natasha Godwin on 7/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 18 - FriendFinder/FriendFinder/FriendFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFinderApp.swift
+//  FriendFinder
+//
+//  Created by Natasha Godwin on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
