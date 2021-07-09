a239e98b2bf367ba4c474b1a967e9f964990d343
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-09 19:12:37 -0400

    Added functionality to GameView

--- 09 - MultiplyThis/MultiplyThis/ContentView.swift
+++ 09 - MultiplyThis/MultiplyThis/ContentView.swift
@@ -7,74 +7,111 @@
 
 import SwiftUI
 
+struct Question: Identifiable {
+    let id = UUID()
+    var text: String
+    var answer: Int
+}
+
 class Game: ObservableObject {
-    @Published var table = 5
-    @Published var numberOfQuestions: Float = 10
+    @Published var tableSelected = 5
+    @Published var numberOfQuestions = 10
+    @Published var questions = [Question]().shuffled()
     
-    var questions: [String] {
-        let sessionLength = 1...Int(numberOfQuestions)
-        var question: String
-        var questions = [String]()
-        
+    func getQuestions()  {
+        let sessionLength = 1...numberOfQuestions
+
         for _ in sessionLength {
-            let randomNumber = Int.random(in: 1...12)
-            question = "What is \(randomNumber) x \(table)?"
-            questions.append(question)
+           let randomElement = Int.random(in: 1...12)
+            let question = "What is \(randomElement) x \(tableSelected)?"
+            let correctAnswer = randomElement * tableSelected
+            
+            questions.append(Question(text: question, answer: correctAnswer))
         }
-        return questions
+    }
+    
+    var randomQuestion: Question {
+        return questions.randomElement() ?? Question(text: "Oops! There was a problem loading your question", answer: 0)
     }
 }
 
 struct ContentView: View {
     @ObservedObject var game = Game()
     @State private var isActive = false
+    
     var body: some View {
         NavigationView {
             Group {
-                if isActive {
-                    PlayGame(game: self.game)
-                } else {
-                    UpdateSettings(game: self.game)
-                }
-            }
-            .navigationBarTitle(isActive ? "Game": "Settings")
+              if isActive == true {
+                  GameView(game: self.game)
+              } else {
+                  SettingsView(game: self.game)
+          }
+         }
+          .navigationBarTitle(isActive ? "Game": "Settings")
             .navigationBarItems(trailing: Button(isActive ? "Settings": "Play") {
-                self.isActive.toggle()
+                isActive.toggle()
             })
         }
     }
-    
 }
+    
 
-struct UpdateSettings: View {
+struct SettingsView: View {
     @ObservedObject var game: Game
+    
     var body: some View {
         Form {
-            Section(header: Text("Practice")) {
-                Stepper("\(game.table) tables", value: $game.table, in: 1...12)
+            Section(header: Text("Tables To Practice")) {
+                Stepper("\(game.tableSelected) tables", value: $game.tableSelected, in: 1...12)
             }
             Section(header: Text("Number of Questions")) {
-                Text("\(game.numberOfQuestions, specifier: "%g") questions")
-                Slider(value: $game.numberOfQuestions, in: 5...20, step: 5) {
-                    Text("Number Of Questions")
-                }
+                Stepper("\(game.numberOfQuestions)", value: $game.numberOfQuestions, in: 1...20)
             }
         }
     }
 }
 
-struct PlayGame: View {
+struct GameView: View {
     @ObservedObject var game: Game
+    @State private var questionNumber = 0
+    @State private var response = ""
+    
+    @State private var points = 0
+
     
     var body: some View {
-        ForEach(game.questions, id: \.self) { question in
-            Text(question)
+        VStack {
+            // TO DO: Show one question
+            Text("Points: \(points)")
+            Text(game.randomQuestion.text)
+            TextField("Your Answer", text: $response, onCommit: nextQuestion)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .keyboardType(.numberPad)
+            Button("Next Question") {
+                nextQuestion()
+            }
+            .disabled(questionNumber > game.numberOfQuestions)
         }
+        .onAppear(perform: game.getQuestions)
+        
+    }
+    
+    func checkAnswer() {
+        // TO DO - Check if the user's answer is correct
     }
     
     func nextQuestion() {
+        questionNumber += 1
+        
+        if questionNumber <= game.numberOfQuestions  {
+            game.questions.shuffle()
+            response = ""
+       }
     }
 }
+    
 
 
 struct ContentView_Previews: PreviewProvider {
