916ddf2c1eac3ccde7939dca2e2ec178aa2cf287
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-29 14:28:17 -0400

    Wrote a method to send and receive orders over the Internet

--- 14 - CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ 14 - CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
 	@ObservedObject var order: Order
+	@State private var confirmationMessage = ""
+	@State private var showingConfirmation = false
     var body: some View {
 		GeometryReader { geometry in
 			 ScrollView {
@@ -22,14 +24,49 @@ struct CheckoutView: View {
 							 .font(.title)
 
 						Button("Place Order") {
-							 // place the order
+							self.placeOrder()
 						}
 						.padding()
 				  }
 			 }
 		}
 		.navigationBarTitle("Check out", displayMode: .inline)
+		.alert(isPresented: $showingConfirmation) {
+			Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("Welcome!")))
+		}
     }
+	
+	// Send and receive orders over the Internet
+	func placeOrder() {
+		// Convert order object to JSON, so it can be sent to a server
+		guard let encoded = try? JSONEncoder().encode(order) else {
+			print("Failed to encode order")
+			return
+		}
+		// Create the URL, wrap it in a URLRequest, and prepare the request to be sent as JSON
+		let url = URL(string: "https://reqres.in/api/cupcakes")!
+		var request = URLRequest(url: url)
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		// Specify how the data should be sent
+		request.httpMethod = "POST"
+		request.httpBody = encoded
+		
+		// Run the request and process the response
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			guard let data = data else {
+				print("No data in response: \(error?.localizedDescription ?? "Unknown")")
+				return
+			}
+			
+			if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+				confirmationMessage = "Your order for \(decodedOrder.quantity) \(Order.cupcakes[decodedOrder.selection].lowercased()) cupcakes is on its way!"
+				showingConfirmation = true
+			} else {
+				print("Invalid response from the server.")
+			}
+		}.resume()
+	}
 }
 
 struct CheckoutView_Previews: PreviewProvider {
