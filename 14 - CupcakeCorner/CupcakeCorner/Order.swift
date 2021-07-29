62c4d9b2378c7515e5321e8f40aa8bf1f82e58bf
Natasha Godwin <natashasgodwin@gmail.com>
2021-07-29 14:00:14 -0400

    Implemented Codable conformance

--- 14 - CupcakeCorner/CupcakeCorner/Order.swift
+++ 14 - CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
 	static let cupcakes = [
 		"Chocolate",
 		"Lemon",
 		"Carrot",
 		"Vanilla"
 	]
+	
 	@Published var selection = 0
 	@Published var quantity = 2
 	@Published var hasSpecialRequest = false {
@@ -59,4 +60,53 @@ class Order: ObservableObject {
 
 		 return cost
 	}
+	
+	// Tells Swift which properties need to be saved
+	enum CodingKeys: CodingKey {
+		case selection
+		case quantity
+		case addExtraFrosting
+		case addSprinkles
+		case name
+		case address
+		case city
+		case zip
+	}
+	
+	// Converts the data to JSON, so it can be sent over the Internet
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		
+		try container.encode(selection, forKey: .selection)
+		try container.encode(quantity, forKey: .quantity)
+
+		try container.encode(addExtraFrosting, forKey: .addExtraFrosting)
+		try container.encode(addSprinkles, forKey: .addSprinkles)
+		
+		try container.encode(name, forKey: .name)
+		try container.encode(address, forKey: .address)
+		try container.encode(city, forKey: .city)
+		try container.encode(zip, forKey: .zip)
+	}
+	
+	// Converts archived data to Swift objects
+	required init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		
+		selection = try container.decode(Int.self, forKey: .selection)
+		quantity = try container.decode(Int.self, forKey: .quantity)
+
+		addExtraFrosting = try container.decode(Bool.self, forKey: .addExtraFrosting)
+		addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+		
+		name = try container.decode(String.self, forKey: .name)
+		address = try container.decode(String.self, forKey: .address)
+		city = try container.decode(String.self, forKey: .city)
+		zip = try container.decode(String.self, forKey: .zip)
+		
+	}
+	
+	// Allows an instance of Order to be created, even if it doesn't have any data
+	init() {}
+	
 }
