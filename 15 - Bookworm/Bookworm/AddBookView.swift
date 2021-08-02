36118609a4e81bc68a3c997ab6f0e58034923e43
Natasha Godwin <natashasgodwin@gmail.com>
2021-08-02 16:22:06 -0400

    Basic UI
    Created data model, built AddBookView, and fetched data in ContentView. Users can now add a book to the database and see the total number of books in ContentView.

---
+++ 15 - Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,61 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Natasha Godwin on 8/2/21.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+	@Environment(\.managedObjectContext) var viewContext
+	@Environment(\.presentationMode) var presentationMode
+	@State private var title = ""
+	@State private var author = ""
+	@State private var genre = ""
+	@State private var rating: Double = 3
+	@State private var review = ""
+	
+	let genres = ["Fiction", "Non-Fiction", "Graphic Novel"]
+	
+    var body: some View {
+		NavigationView {
+			Form {
+				Section(header: Text("Basics")) {
+					TextField("Title", text: $title)
+					TextField("Author", text: $author)
+					Picker("Genre", selection: $genre) {
+						ForEach(genres, id: \.self) {
+							Text($0)
+						}
+					}
+					.pickerStyle(SegmentedPickerStyle())
+				}
+				Section(header: Text("Review")) {
+					Stepper("\(rating, specifier: "%g") stars", value: $rating, in: 1...5, step: 0.5)
+					TextEditor(text: $review)
+				}
+			}
+			.navigationBarTitle(title.isEmpty ? "New Book": "\(title)")
+			.navigationBarItems(trailing: Button("Save") {
+				let newBook = Book(context: self.viewContext)
+				newBook.title = self.title
+				newBook.author = self.author
+				newBook.genre = self.genre
+				newBook.rating = Int16(self.rating)
+				newBook.review = self.review
+				
+				try? self.viewContext.save() // save changes
+				
+				self.presentationMode.wrappedValue.dismiss()
+				
+			})
+		}
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- 15 - Bookworm/Bookworm/ContentView.swift
+++ 15 - Bookworm/Bookworm/ContentView.swift
@@ -5,12 +5,37 @@
 //  Created by Natasha Godwin on 6/16/21.
 //
 
+/*
+
+@Environment
+A property wrapper that allows you to read values from the environment.
+
+.environment()
+A modifier that allows you to write values in the environment. Takes two parameters: a key to write to and the value that needs to be sent in.
+
+Use Cases:
+- Passing a managed object context from a view's environment to a sheet, so new items can be added
+*/
+
 import SwiftUI
 import CoreData
 
 struct ContentView: View {
+	@Environment(\.managedObjectContext) var viewContext
+	@FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+	@State private var addingBook = false
 	var body: some View {
-		Text("Hello, World!")
+		NavigationView {
+			Text("\(books.count) books")
+			.navigationBarTitle("Bookworm")
+			.navigationBarItems(trailing: Button("Add") {
+				self.addingBook.toggle()
+			})
+		}
+		.sheet(isPresented: $addingBook) {
+			AddBookView()
+				.environment(\.managedObjectContext, self.viewContext)
+		}
 	}
 }
 
