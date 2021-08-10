2980cd2acd50648dc452ba5c4d1ba61022994d2f
Natasha Godwin <natashasgodwin@gmail.com>
2021-08-10 11:28:09 -0400

    Built a generic filtered list

--- 16 - CoreDataProject/CoreDataProject/View/Examples/FilteredList.swift
+++ 16 - CoreDataProject/CoreDataProject/View/Examples/FilteredList.swift
@@ -6,6 +6,39 @@
 //
 
 import SwiftUI
+import CoreData
+
+/*
+
+Imagine you're creating a filtered list with generics and Core Data.
+
+What three things do you need to do?
+
+Will this code work: NSPredicate(format: "%@ BEGINSWITH %@", keyName, filter)?
+
+No. When you use %@, Core Data automatically inserts quote marks around the content.
+Attributes shouldn't have quotes around them.
+
+You should write this instead:
+NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue)
+
+What does %K do?
+%K is a special symbol in NSPredicate that can be used to replace attribute names.
+Unlike %@, %K will insert the provided value without putting quote marks around it.
+
+This is useful when creating generic filters.
+
+Example:
+NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue)
+
+
+Lingering Questions
+What does @ViewBuilder do?
+What does @escaping do?
+
+
+
+*/
 
 struct Notes: View {
 	@Environment(\.managedObjectContext) var viewContext
@@ -20,7 +53,18 @@ struct Notes: View {
 				  filter = "quotes"
 			  }
 		  }
-		  FilteredList(filter: filter)
+		  //FilteredList(filter: filter)
+		GenericFilteredList(filterKey: "category", filterValue: filter) { (note: Note) in
+			VStack(alignment: .leading, spacing: 10) {
+				Text(note.text ?? "")
+					.font(.headline)
+				Text(note.category ?? "uncategorized")
+					.frame(width: 100, height: 20)
+					.background(Color.blue)
+					.foregroundColor(.white)
+					.clipShape(RoundedRectangle(cornerRadius: 25.0))
+			}
+		}
 		  Button("Add") {
 			 let note1 = Note(context: self.viewContext)
 			 note1.text = "'The one who does the work does the learning' - Terry Doyle"
@@ -74,3 +118,23 @@ struct FilteredList: View {
 		
     }
  }
+
+struct GenericFilteredList<T: NSManagedObject, Content: View>: View {
+	 var fetchRequest: FetchRequest<T> // Whatever is passed in must be an NSManagedObject (Core Data subclass)
+	 var results: FetchedResults<T> { fetchRequest.wrappedValue }
+
+	 let content: (T) -> Content // Let the containing view decide what the content is
+
+	 var body: some View {
+		  List(results, id: \.self) { result in
+				self.content(result)
+		  }
+	 }
+
+	 init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+		  fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+		  self.content = content
+	 }
+}
+
+
