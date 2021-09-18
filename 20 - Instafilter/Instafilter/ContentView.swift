4d175d9fe25dfe8916c548dd0e08dad0265b4de3
Natasha Godwin <natashasgodwin@gmail.com>
2021-09-18 11:58:30 -0400

    Built the UI

--- 20 - Instafilter/Instafilter/ContentView.swift
+++ 20 - Instafilter/Instafilter/ContentView.swift
@@ -7,11 +7,64 @@
 
 import SwiftUI
 
+/*
+What types of control flow does SwiftUI support?
+SwiftUI only supports if/else statements inside of the body property.
+You cannot use if let, for, while, or switch.
 
+What happens under the hood when you use an if/else statement inside the body property?
+Swift converts the statement into an internal view type called ConditionalContent, which
+stores the condition and true and false views. Swift checks which one to display at run time.
+
+This is why if let and switch statements don't work. If let creates a constant and switch statements can have a number of cases.
+
+
+*/
 
 struct ContentView: View {
+	@State private var image: Image?
+	@State private var filterIntensity = 0.5
 	 var body: some View {
-		  Text("Hello, World!")
+		NavigationView {
+			VStack {
+				ZStack {
+					Rectangle()
+						.fill(Color.secondary)
+					// display the image
+					if image != nil {
+						image?
+							.resizable()
+							.scaledToFit()
+					} else {
+						Text("Tap to select picture")
+							.foregroundColor(.white)
+							.font(.headline)
+					}
+				}
+				.onTapGesture {
+					// select an image
+				}
+				
+				HStack {
+					Text("Intensity")
+					Slider(value: $filterIntensity)
+				}.padding(.vertical)
+				
+				HStack {
+					Button("Change Filter") {
+						// change filter
+					}
+					
+					Spacer()
+					
+					Button("Save") {
+						// save the picture
+					}
+				}
+			}
+			.padding([.horizontal, .bottom])
+			.navigationBarTitle("Instafilter")
+		}
 	 }
 }
 
