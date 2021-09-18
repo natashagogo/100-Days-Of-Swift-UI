9b4b515a4b41850a372aaf299f2180f998036cc8
Natasha Godwin <natashasgodwin@gmail.com>
2021-09-18 13:09:40 -0400

    Edited comments, organized files

--- 20 - Instafilter/Instafilter/ImagePicker.swift
+++ 20 - Instafilter/Instafilter/ImagePicker.swift
@@ -10,28 +10,22 @@ import UIKit
 
 /*
 In order to use UIKit in this application, we need to ...
-1. Create a UIViewController, such as UIImagePickerController, inside a SwiftUI struct.
+1. Create a SwiftUI struct that wraps a UIViewController, such as UIImagePickerController
    This struct should conform to the UIViewControllerRepresentable protocol and have two methods:
    - makeUIViewController()
    - updateUIViewController()
    Only the first one, which creates the UIViewController, needs to be implemented
    When an instance of the struct is created, Swift automatically calls this method to create the UIViewController.
-2. Create a coordinator class.
-   This class acts as a delegate for the UIKit components, receiving notifications any time the UIViewController changes.
-	It belongs to the SwiftUI struct, passing any relevant information back up to it when needed.
-   Because of that, the coordinator class should conform to NSObject, the base class for most types in UIKit, along with any delegate protocols that are required.
+2. Create a coordinator class inside the SwiftUI struct.
+   This class acts as a delegate for the UIViewController, receiving notifications any time it changes.
+	It belongs to the SwiftUI struct and passes any relevant information back up to it when needed.
+   Because of that, the coordinator class should conform to NSObject, the base class for most types in UIKit, along with any delegate protocols that are required for the UIViewController you're using.
 3. Make the coordinator class the delegate of the UIViewController in the makeUIViewController() method.
 4. Implement the makeCoordinator() method.
    This method lets you control how a coordinator is made.
    At a minimum, you should tell Swift to use the coordinator class for the SwiftUI struct's coordinator.
 5. Give the coordinator some sort of functionality (whatever is required by the UIViewController class)
 
-What is a delegate?
-A custom class that is notified any time a UIViewController changes.
-Note that it usually needs to conform to one or more protocols.
-
-What does the didFinishPickingMediaWithInfo method do?
-The UIImagePickerController method gets information about a selected image.
 */
 
 struct ImagePicker: UIViewControllerRepresentable {
