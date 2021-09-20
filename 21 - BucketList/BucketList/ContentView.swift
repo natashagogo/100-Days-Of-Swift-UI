465a4cdcf5c8fbd418fc0d52ef456716900cb6fe
Natasha Godwin <natashasgodwin@gmail.com>
2021-09-20 12:44:00 -0400

    Initial files

---
+++ 21 - BucketList/BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Natasha Godwin on 9/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
