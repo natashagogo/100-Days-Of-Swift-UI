079993a9ff4f50c188b888844c1fb4601775aa27
Natasha Godwin <natashasgodwin@gmail.com>
2021-09-26 13:41:28 -0400

    Edited comments

--- 21 - BucketList/BucketList/ContentView.swift
+++ 21 - BucketList/BucketList/ContentView.swift
@@ -8,34 +8,6 @@
 import SwiftUI
 import MapKit
 
-/*
-
-What are some good examples of when you should make an extension conform to ObservableObject?
- 
-Adding a pin to a map is a good use case. In MapKit, annotations are of type MKPointAnnotation; and the title and subtitle properties for this type are optionals.
-
-This creates problem when adding editing functionality in a SwiftUI app: the title and subtitle properties are optionals, but text fields can't contain optionals.
-
-To give the user the option of updating these properties, you can write an extension on MKPointAnnotation that unwraps each property and use property observers to get and set its values.
-
-From there, you can add an @ObservedObject property of type MKPointAnnotation to the edit view, so its values can be updated.
- 
-
- Is it possible for an object to conform to ObservableObject and not have @Published properties?
- 
- Yes. You can use property observers instead, and depending on performance considerations, it may be better to do so.
- With an @Published property wrapper, the view refreshes every time its value changes. Properties are read as changes are made. In some cases, like when allowing users to edit annotations on a map, this isn't desirable.
-
-How can you get a list of nearby locations?
-Query Wikipedia using GPS coordinates and it will send back a list of places that are nearby. 
-
-You can mix and match Text views with different styles using the + operator
-Text("Title").italic() + Text("by") + Text("Author").bold()
-
-
-*/
-
-
 struct ContentView: View {
 	@State private var centerCoordinate = CLLocationCoordinate2D()
 	@State private var locations = [MKPointAnnotation()]
