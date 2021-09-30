0e5cbfc6258a87a3d522689e9ee11db395174f7a
Natasha Godwin <natashasgodwin@gmail.com>
2021-09-30 11:28:16 -0400

    Accessibility improvements
    Created a custom label for words on the used words list, so it sounds more natural to VoiceOver users.

--- 07 - WordScramble/WordScramble/ContentView.swift
+++ 07 - WordScramble/WordScramble/ContentView.swift
@@ -36,9 +36,13 @@ struct ContentView: View {
                     .font(.headline)
                     .foregroundColor(.blue)
 
-                List(usedWords, id: \.self) {
-                    Image(systemName: "\($0.count).circle")
-                    Text($0)
+                List(usedWords, id: \.self) { word in
+						HStack {
+							Image(systemName: "\(word.count).circle")
+							Text(word)
+						}
+						.accessibilityElement(children: .ignore)
+						.accessibility(label: Text("\(word), \(word.count) letters"))
                 }
                 
             }
