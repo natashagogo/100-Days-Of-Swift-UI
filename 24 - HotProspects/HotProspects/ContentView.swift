0da4343db70cbccd75162895a4222c385fa2dc32
Natasha Godwin <natashasgodwin@gmail.com>
2021-10-06 17:58:34 -0400

    Edited comments

--- 24 - HotProspects/HotProspects/ContentView.swift
+++ 24 - HotProspects/HotProspects/ContentView.swift
@@ -7,25 +7,6 @@
 
 import SwiftUI
 
-/*
-"The most difficult thing is the decision to act, the rest is merely tenacity." - Amelia Earhart
-
-map()
-A method that iterates over elements in a sequence, transforms each using a closure, and returns the result in a new array.
-
-filter()
-A method that iterates over elements in a sequence, checks whether each meets a certain condition, and returns those that do in a new array
-
-functional programming
-
-What does the .environmentObject modifier do?
-It posts an ObservableObject into the SwiftUI environment, so all child views can access it. Note that you will have to add this directly to views inside a sheet.
-
-What does @EnvironmentObject do?
-This property wrapper finds an object, attaches it to a property, and keeps it up to date over time.
-
-
-*/
 
 struct ContentView: View {
 	var prospects = Prospects()
