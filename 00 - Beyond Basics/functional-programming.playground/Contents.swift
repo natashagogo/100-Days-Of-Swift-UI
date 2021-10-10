3672a7470f64c54a1e686669a8e9d4e9ac8e4814
Natasha Godwin <natashasgodwin@gmail.com>
2021-10-10 14:35:07 -0400

    Added notes

--- 00 - Beyond Basics/functional-programming.playground/Contents.swift
+++ 00 - Beyond Basics/functional-programming.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+/*
+Lightweight encounters (Andy Matuschak)
+Finding a simple, practical way to apply theory.
+
+Principles of Functional Programming
+1. Action: Use verbs, not nouns. Treat functions as first-class data types.
+2. Modularity: Treat functions like legos: use small functions to create more complex behavior
+3. Predictability: The same input must return the same output.
+4. Immutability: Use immutable value types.
+5. Containment: Avoid using state inside a function whenever possible.
+
+Higher-order function
+A function that accepts another function as a parameter
+
+Pure function
+A function that always returns the same output, given the same input, without affecting other functions, i.e. creating "side-effects).
+
+*/
