5bdffa72d6830464bafb29535064ba23fc2ba2c5
Natasha Godwin <natashasgodwin@gmail.com>
2021-10-31 15:30:15 -0400

    Initial Files

---
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Natasha Godwin on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Natasha Godwin on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
