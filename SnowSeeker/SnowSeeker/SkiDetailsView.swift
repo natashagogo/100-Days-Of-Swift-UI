73415e50487cebb2a2e674b93fd8728b695f03ce
Natasha Godwin <natashasgodwin@gmail.com>
2021-11-03 14:34:06 -0400

    Built SkiDetailsView
    This displays the resort's elevation and snow depth.

---
+++ SnowSeeker/SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,24 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Natasha Godwin on 11/3/21.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+	let resort: Resort
+    var body: some View {
+		VStack {
+			Text("Elevation: \(resort.elevation)m")
+			Text("Snow: \(resort.snowDepth)cm")
+		}
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+		SkiDetailsView(resort: Resort.example)
+    }
+}
