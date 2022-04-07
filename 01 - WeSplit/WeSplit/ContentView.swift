7e3c632d6e2da132b7914c6cd02eda6a0323a68c
Natasha Godwin <natashasgodwin@gmail.com>
2022-04-07 11:47:58 -0400

    Explored techniques
    Explored all of the techniques for this project: forms, state, two-way data binding, creating views with ForEach, and pickers.

--- 01 - WeSplit/WeSplit/ContentView.swift
+++ 01 - WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 
 struct ContentView: View {
+	let interns: [String] = [
+		"Meredith Grey",
+		"Christina Yang",
+		"Alex Karev",
+		"Izzy Stevens",
+		"George O'Malley"
+	]
+	
+	@State private var onCall = "Meredith Grey"
+	
     var body: some View {
-		 Text("Hello, SwiftUI!")
+		 Form {
+			 Section(header: Text("Interns")) {
+				 Picker("Interns", selection: $onCall) {
+					 ForEach(interns, id: \.self) { intern in
+						 Text(intern)
+					 }
+				 }
+				 .pickerStyle(WheelPickerStyle())
+	
+			 }
+			 Section(header: Text("On Call")) {
+				 Text(onCall)
+			 }
+		 }
 	 }
 }
 
