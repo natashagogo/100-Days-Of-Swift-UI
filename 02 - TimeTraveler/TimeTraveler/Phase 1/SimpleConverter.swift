ded8dd6a8d18c3fc8916118d1ef30959861c8565
Natasha Godwin <natashasgodwin@gmail.com>
2022-04-10 17:50:51 -0400

    Reset
    Implementing a new solution.

---
+++ 02 - TimeTraveler/TimeTraveler/Phase 1/SimpleConverter.swift
@@ -0,0 +1,99 @@
+//
+//  SimpleConverter.swift
+//  TimeTraveler
+//
+//  Created by Natasha Godwin on 4/10/22.
+//
+
+import SwiftUI
+
+struct SimpleConverter: View {
+	 @State private var entry = ""
+	 @State private var inputSelected = 1 // index number
+	 @State private var outputSelected = 0 // index number
+	 
+	 let units: [String] = [
+		  "minutes",
+		  "hours",
+		  "days"
+	 ]
+	 
+	 var convertNumber: Double {
+		  let userEntry = Double(entry) ?? 0
+		  let result: Double
+		  
+		  let minutesInHour: Double = 60
+		  let minutesInDay: Double = minutesInHour * 24
+		  
+		  let inputSelection = units[inputSelected]
+		  let outputSelection = units[outputSelected]
+		  
+		  let minute = units[0]
+		  let hour = units[1]
+		  let day = units[2]
+		  
+		  switch (inputSelection, outputSelection) {
+			 case (minute, hour):
+				result = userEntry / minutesInHour
+			 case (minute, day):
+				result = userEntry / minutesInDay
+			 case (hour, day):
+				result = userEntry * minutesInHour / minutesInDay
+			 case (hour, minute):
+				result = userEntry * minutesInHour
+			 case (day, minute):
+				result = userEntry * minutesInDay
+			 case (day, hour):
+				result = userEntry * minutesInDay / minutesInHour
+			 default:
+				result = userEntry
+		  }
+		  
+		  
+		  return result
+	 
+	 }
+	 
+	 var body: some View {
+		  NavigationView {
+				Form {
+					 Section {
+						  TextField("Number", text: $entry)
+							 .keyboardType(.decimalPad)
+					 }
+					 
+					 Section(header: Text("Input Unit")) {
+						  Picker("Input Unit", selection: $inputSelected) {
+							  ForEach(units, id: \.self) { unit in
+									 Text(unit)
+								}
+						  }
+						  .pickerStyle(SegmentedPickerStyle())
+					 }
+					 
+					 Section(header: Text("Output Unit")) {
+						  Picker("Output Unit", selection: $outputSelected) {
+							  ForEach(units, id: \.self) { unit in
+									 Text(unit)
+								}
+						  }
+						  .pickerStyle(SegmentedPickerStyle())
+					 }
+					 
+					 Section(header: Text("Result")) {
+						  Text("There are \(convertNumber, specifier: "%.2f") \(units[outputSelected]) in \(entry) \(units[inputSelected])")
+					 }
+					 
+					 
+				}
+				.navigationTitle("Time Traveler")
+		  }
+	 }
+}
+
+struct SimpleConverter_Previews: PreviewProvider {
+	 static var previews: some View {
+		  SimpleConverter()
+	 }
+}
+
