7e034455d94294224b9e407d98bd18275a8c6831
Natasha Godwin <natashasgodwin@gmail.com>
2022-04-15 14:44:49 -0400

    Reset

--- 04 - ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 04 - ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,25 +7,9 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-}
-
-
 struct ContentView: View {
     var body: some View {
         Text("Hello, SwiftUI!")
-            .titleStyle()
     }
 }
 
