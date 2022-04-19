bf255738c08bd0e1efdd2ecb50a08ff2c1459655
Natasha Godwin <natashasgodwin@gmail.com>
2022-04-19 17:40:51 -0400

    Added technique examples, ML model

--- 06 - BetterRest/BetterRest/ContentView.swift
+++ 06 - BetterRest/BetterRest/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 /*
     NOTES
- 
     This app would be more useful if it told you how many cups of coffee to drink, based on your desired wake-up time.
 */
 
 
 struct ContentView: View {
+	@State private var sleepAmount = 8.0
+	@State private var wakeUp = Date.now
+	
     var body: some View {
-        Text("Hello, Sleep!")
+		 Form {
+			 Text(wakeUp.formatted(date: .long, time: .shortened))
+			 Text(wakeUp, format: .dateTime.hour().minute())
+			 DatePicker("Date", selection: $wakeUp, in: Date.now...)
+				 .labelsHidden()
+			 Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...20, step: 0.25)
+		 }
     }
 }
 
