1ab17c045aeb145bbd5103a173a233c6cd0deb93
Natasha Godwin <natashasgodwin@gmail.com>
2022-04-27 15:33:18 -0400

    Built the UI

--- 07 - WordScramble/WordScramble/ContentView.swift
+++ 07 - WordScramble/WordScramble/ContentView.swift
@@ -9,10 +9,43 @@ import SwiftUI
 
 
 struct ContentView: View {
+	@State private var usedWords = [String]()
+	@State private var rootWord = ""
+	@State private var newWord = ""
+	
     var body: some View {
-        Text("Hello, World!")
+		 NavigationView {
+			 List {
+				 Section {
+					 TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+						 .autocapitalization(.none)
+				 }
+				 Section {
+					 ForEach(usedWords, id: \.self) { word in
+						 HStack {
+							 Image(systemName: "\(word.count).circle.fill")
+								 .foregroundColor(word.count > 5 ? Color.blue: Color.gray)
+	
+							 Text(word)
+						 }
+					 }
+				 }
+			 }
+			 .navigationTitle(rootWord)
+			 .onSubmit(addNewWord)
+		 }
     }
-    
+	
+	func addNewWord() {
+		let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+		
+		guard word.count > 0 else { return }
+		
+		withAnimation {
+			usedWords.insert(word, at: 0)
+		}
+		newWord = ""
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
