bfb7ee0a83b4cd212116e999ab1a6b62fd41e923
Natasha Godwin <63130264+natashagodwin@users.noreply.github.com>
2024-06-17 13:09:00 -0400

    Added Checkpoint 3

--- 00 - Swift/SwiftBasics.playground/Contents.swift
+++ 00 - Swift/SwiftBasics.playground/Contents.swift
@@ -42,3 +42,16 @@ let datesByMonth = Dictionary(grouping: dates, by: { Calendar.current.dateCompon
 
 print(datesByMonth.keys.count)
 
+// Checkpoint 3: Loops (FizzBuzz)
+
+for number in 1...100 {
+    if number.isMultiple(of: 3) && number.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if number.isMultiple(of: 3) {
+        print("Fizz")
+    } else if number.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(number)
+    }
+}
